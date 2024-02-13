ac682f6c5a658fcc16ae1e85154a56e80976e2d6
Chalky23 <139451646+Chalky23@users.noreply.github.com>
2024-02-13 14:29:59 +0000

    added first part of real world problems

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-let songs = ["song1", "song2", "song3", "song4", "song5"];
-songs[4] = "baby shark"
+// let songs = ["song1", "song2", "song3", "song4", "song5"];
+// songs[4] = "baby shark"
 
-let cookiesOrdered = [15, 3, 67, 1, 22, 100];
+// let cookiesOrdered = [15, 3, 67, 1, 22, 100];
 
-for (let i = 0; i < cookiesOrdered.length; i++){
-    cookiesOrdered[i] = cookiesOrdered[i] * 10
-    console.log(cookiesOrdered[i])
+// for (let i = 0; i < cookiesOrdered.length; i++){
+    //cookiesOrdered[i] = cookiesOrdered[i] * 10
+    //console.log(cookiesOrdered[i])
+//}
+
+let questions = ["What is Igor's surname?","What is 1 plus 1?", "What colour is grass?", "What is the capital of England?", "What is 2 times 2?"]
+
+for (let i=0; i < questions.length; i++) {
+    console.log `${i+1}: ${questions[i]}`
 }
