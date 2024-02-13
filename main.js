20ffb435cc4cc6ad9fdd584bfb1746a540983bda
Igor Silva <igorsilvauk@gmail.com>
2024-02-13 15:40:11 +0000

    Final Ticket added (quizgame)

--- main.js
+++ main.js
@@ -8,8 +8,27 @@
     //console.log(cookiesOrdered[i])
 //}
 
-let questions = ["What is Igor's surname?","What is 1 plus 1?", "What colour is grass?", "What is the capital of England?", "What is 2 times 2?"]
+let questions = ["What is Igor's surname?","What is 1 plus 1?", "What colour is grass?", "What is the capital of England?", "What is 2 times 2?"];
+let answer    = ['Silva', 2, 'Green', 'London', 4];
 
-for (let i=0; i < questions.length; i++) {
-    console.log `${i+1}: ${questions[i]}`
+function plaQuiz() {
+    confirm(`You've started the 5 questions quiz!`)
+    let qCount = 1;
+    let score = 0;
+
+    for (let i = 0; i < questions.length; i++) {
+        let quiz = prompt(`Question n.${qCount++} : ${questions[i]}`)
+        if (quiz == answer[i]){
+            score++
+            confirm(`Correct answer! You've earned a point. Your new score is: ${score}`)
+            if (i === 4){
+                confirm(`Congratulations!! You finished the 5 questions quiz. Your score is: ${score}`)
+            }
+        }else {
+            confirm(`Wrong answer. You didn't get a point. Your score is: ${score}`)
+            if (i === 4){
+                confirm(`Congratulations!! You finished the 5 questions quiz. Your score is: ${score}`)
+            }
+        }
+    }
 }
