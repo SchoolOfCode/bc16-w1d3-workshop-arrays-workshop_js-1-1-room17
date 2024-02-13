0313ec0a99198694d0d6f30c4d14e7e17c1e828a
Igor Silva <igorsilvauk@gmail.com>
2024-02-13 14:20:03 +0000

    ticke5 added

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 let songs = ["song1", "song2", "song3", "song4", "song5"];
 songs[4] = "baby shark"
+
+let cookiesOrdered = [15, 3, 67, 1, 22, 100];
+
+for (let i = 0; i < cookiesOrdered.length; i++){
+    cookiesOrdered[i] = cookiesOrdered[i] * 10
+    console.log(cookiesOrdered[i])
+}
